87214c6a3c418b129367f6d8abb1b280996d7694
Your Name <rsagadatov@gmail.com>
2021-08-15 20:21:59 +0500

    #2 добавлена возможность регистрации пользователя

--- database.py
+++ database.py
@@ -10,32 +10,51 @@ def sql_query(query):
         except Error as e:
             print(e)
         result = cursor.fetchall()
+        cursor.close()
         return result
 
 
 def create_tables():
-    users_query = """CREATE TABLE IF NOT EXISTS users 
+    users_query = '''CREATE TABLE IF NOT EXISTS users 
                         (user_id INTEGER PRIMARY KEY NOT NULL,
-                        user_name TEXT
-                        name TEXT);"""
-    tasks_query = """CREATE TABLE IF NOT EXISTS tasks 
+                        user_name TEXT,
+                        name TEXT);'''
+    tasks_query = '''CREATE TABLE IF NOT EXISTS tasks 
                         (task_id INTEGER PRIMARY KEY NOT NULL,
                         task_name TEXT,
                         task_date TEXT,
-                        user_id INTEGER FOREIGN KEY (fk_user_id) REFERENCES users (user_id);"""
+                        user_id INTEGER,
+                        FOREIGN KEY (user_id) REFERENCES users (user_id));'''
     sql_query(users_query)
     sql_query(tasks_query)
 
 
+def user_exist(user_id):
+    exist = sql_query(f'SELECT * FROM users WHERE user_id = {user_id}')
+    return exist
+
+
+def task_exist(user_id):
+    exist = sql_query(f'SELECT * FROM tasks WHERE user_id = {user_id}')
+    return exist
+
+
 def add_user(user_id, user_name, name):
-    user_exist = sql_query(f"SELECT FROM users WHERE user_id = {user_id}")
-    if not user_exist:
-        insert_user_query = f'INSERT INTO users (user_id, user_name, name) \
-                            VALUES ({user_id}, "{user_name}", "{name}");'
+    if not user_exist(user_id):
+        insert_user_query = f'INSERT INTO users (user_id, user_name, name)' \
+                            f'VALUES ({user_id}, "{user_name}", "{name}");'
         sql_query(insert_user_query)
 
 
 def add_task(user_id, task_name, task_date):
-    insert_task_query = f'INSERT INTO tasks (task_id, task_name, task_date, user_id)\
-                        VALUES (Null, "{task_name}", "{task_date}", {user_id});'
+    insert_task_query = f'INSERT INTO tasks (task_id, task_name, task_date, user_id)'\
+                        f'VALUES (Null, "{task_name}", "{task_date}", {user_id});'
     sql_query(insert_task_query)
+
+
+def check_all_users():
+    check_all_query = f'SELECT * FROM users'
+    sql_query(check_all_query)
+
+
+
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import telebot
+from telebot import types
+import database
 from config import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
+database.create_tables()
+database.check_all_users()
 
 
 @bot.message_handler(commands=['start'])
@@ -10,8 +14,33 @@ def send_welcome(message):
 
 
 @bot.message_handler(commands=['help'])
-def send_welcome(message):
-    bot.send_message(message.from_user.id, f"пока что тут пусто(")
+def send_help(message):
+    register_button = types.InlineKeyboardButton('регистрация', callback_data='register')
+    add_task_button = types.InlineKeyboardButton('добавить задачу', callback_data='add_task')
+    delete_task_button = types.InlineKeyboardButton('удалить задачу', callback_data='delete_task')
+    if not database.user_exist(message.from_user.id):
+        buttons = types.InlineKeyboardMarkup().add(register_button)
+        bot.send_message(message.from_user.id, f"Доступные команды:", reply_markup=buttons)
+
+    elif not database.task_exist(message.from_user.id):
+        buttons = types.InlineKeyboardMarkup().add(add_task_button)
+        bot.send_message(message.from_user.id, f"Доступные команды:", reply_markup=buttons)
+
+    else:
+        buttons = types.InlineKeyboardMarkup().add(add_task_button, delete_task_button)
+        bot.send_message(message.from_user.id, f"Доступные команды:", reply_markup=buttons)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'register')
+def register_user(call):
+    ask_a_question = bot.send_message(call.from_user.id, f"Как мне к вам обращаться?")
+    bot.register_next_step_handler(ask_a_question, get_name)
+
+
+def get_name(message):
+    name = message.text
+    database.add_user(message.from_user.id, message.from_user.username, name)
+    bot.send_message(message.from_user.id, "Готово, теперь можно планировать задачи.")
 
 
 @bot.message_handler(content_types=['text'])
